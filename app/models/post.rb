1edc92aa7789c83e650916fd0a5f65822532571d
aprietof <adrianprieto00@gmail.com>
2016-04-09 17:30:40 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,27 @@
 class Post
 
+  attr_accessor :title, :content
+
   def self.create_table
-    DB.execute("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY)")
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS posts (
+      id INTEGER PRIMARY KEY,
+      title TEXT,
+      content TEXT
+      );
+    SQL
+
+    DB.execute(sql)
   end
+
+  def insert
+    sql = <<-SQL
+      INSERT INTO posts (title, content) VALUES (?, ?);
+
+    SQL
+
+    DB.execute(sql, self.title, self.content)
+  end
+
 
 end
