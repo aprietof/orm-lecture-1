ba8d53a81bcd5e4cceb61dbd5724aaab00fba2b5
aprietof <adrianprieto00@gmail.com>
2016-04-09 23:11:43 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,24 @@
 class Post
 
   attr_accessor :title, :content
+  attr_reader :id
+
+  def initialize(id = nil)
+    @id = id
+  end
+
+  def self.all
+    results = DB.execute("SELECT * FROM posts")
+
+    results.collect do |row|
+      Post.new_from_db(row)
+    end
+  end
 
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS posts (
-      id INTEGER PRIMARY KEY,
+      id INTEGER PRIMARY KEY
       title TEXT,
       content TEXT
       );
@@ -14,14 +27,45 @@ class Post
     DB.execute(sql)
   end
 
-  def insert
-    sql = <<-SQL
-      INSERT INTO posts (title, content) VALUES (?, ?);
-
-    SQL
-
-    DB.execute(sql, self.title, self.content)
+  def self.new_from_db(row)
+    Post.new(row[0]).tap do |p|
+      p.title   = row[1]
+      p.content = row[2]
+    end
   end
+
+  def ==(other_post)
+    self.id == other_post.id
+  end
+
+  def persisted?
+    self.id
+  end
+
+  def save
+    persisted? ? update : insert
+    # if persisted?
+    #   update
+    # else
+    #   insert
+    # end
+  end
+
+  private
+    def insert
+      sql = <<-SQL
+        INSERT INTO posts (title, content) VALUES (?, ?);
+      SQL
+
+      DB.execute(sql, self.title, self.content)
+    end
+
+    def update
+      sql = <<-SQL
+        UPDATE posts SET title = ?, content = ? WHERE id = ?
+      SQL
+      DB.execute(sql, self.title, self.content, self.id)
+    end
 
 
 end
